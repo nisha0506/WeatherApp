aa6d3bbaef03163253518d35b4262e0dc08929a7
Nisha Singh <80663480+nisha0506@users.noreply.github.com>
2024-03-28 12:16:14 +0530

    complete weather app

---
+++ public/js/main.js
@@ -0,0 +1,48 @@
+const submitbtnEl = document.getElementById("submitbtn")
+const cityName = document.getElementById("cityname")
+const city_name = document.getElementById("city_name")
+const temp_status = document.getElementById("temp_status")
+const temp_real_val = document.getElementById("temp_real_val")
+const datahide = document.querySelector('.middle_layer')
+
+submitbtnEl.addEventListener("click",async(event) =>{
+    event.preventDefault()
+    let cityval = cityName.value;
+    if(cityval === ""){
+        city_name.innerText = `Please enter a name before you search`;
+        datahide.classList.add('data_hide')
+    }else{
+        try{
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityval}&units=metric&appid=834fa9ea467841cf01effa730e129b43`;
+        const response = await fetch(url)
+        const data = await response.json()
+        const arr = [data]
+        city_name.innerText = `${arr[0].name}, ${arr[0].sys.country}`
+        temp_real_val.innerText = arr[0].main.temp;
+        const temp_mood = arr[0].weather[0].main;
+
+        if(temp_mood === "Clear")
+        {
+            temp_status.innerHTML = 
+            "<i class='fas fa-sun' style='color: #eccc68;'></i>";
+        }
+        else if(temp_mood === "Clouds"){
+            temp_status.innerHTML = 
+            "<i class='fas fa-cloud' style='color: #f1f2f6;'></i>";
+        }
+        else if(temp_mood === "Rain"){
+            temp_status.innerHTML = 
+            "<i class='fas fa-cloud-rain' style='color: #a4b0be;'></i>";
+        }
+        else{
+            temp_status.innerHTML = 
+            "<i class='fas fa-cloud' style='color: #f1f2f6;'></i>";
+        }
+        datahide.classList.remove('data_hide');
+        }
+        catch{
+            city_name.innerText =`Please enter name properly`;
+            datahide.classList.add('data_hide');
+        }
+    }
+})
---
+++ src/index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = process.env.PORT || 3000; //if some other operation is going on at 3000 port then it will transfer it to another port
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.render("index.ejs")
+  });
+  
+app.get("/about",(req,res)=>{
+    res.render("about.ejs")
+});
+
+app.get("/weather",(req,res)=>{
+    res.render("weather.ejs")
+});
+
+app.get("*",(req,res)=>{
+    res.render("error.ejs",{
+        errorMsg:"Oops page not found",
+    })
+});
+
+app.listen(port,()=>{
+    console.log(`listening at port ${port}`)
+});
